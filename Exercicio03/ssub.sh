ad0e558418fc3e8529900835a7a4edf63dca3c62
Ghust1995 <gustavo.guimaraes@sdumont13.bullx>
2017-08-30 01:50:42 -0300

    correct output

--- Exercicio03/ssub.sh
+++ Exercicio03/ssub.sh
@@ -2,14 +2,14 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
+#SBATCH --cpus-per-task=4
 #SBATCH -p cpu_dev
-#SBATCH -J C_1
+#SBATCH -J C_4
 #SBATCH --time=00:02:00
-#SBATCH --output=/scratch/ce2652016/gustavo.guimaraes/ce265/Exercicio03/Out_Crivo_1_OMP_O3
+#SBATCH --output=/scratch/ce2652016/gustavo.guimaraes/ce265/Exercicio03/Out_Crivo_4_OMP_O3
 #SBATCH --exclusive
 
 cd $SLURM_SUBMIT_DIR
 ulimit -s unlimited
-export OMP_NUM_THREADS=1
+export OMP_NUM_THREADS=4
 srun -n $SLURM_NTASKS /scratch/ce2652016/gustavo.guimaraes/ce265/Exercicio03/Crivo.exe 1000000000
