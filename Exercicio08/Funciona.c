7ceb2ded604bca434e7114d25d76f4467d4340ce
Gustavo Guimaraes <gustavo.ceci95@gmail.com>
2017-10-24 23:38:11 -0200

    added to main

--- Exercicio08/Funciona.c
+++ Exercicio08/Funciona.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 #include "mpi.h"
 
-void InitTabul(int* , int* , int , int , int );
-void DumpTabul(int * , int , int , int , char* , int , int , int , int );
-void UmaVida(int*, int*, int);
-int Correto(int* , int , int , int , int );
+void InitTabul(int *, int *, int, int, int);
+void DumpTabul(int *, int, int, int, char*, int, int, int, int);
+void UmaVida(int *, int *, int, int, int, int, int);
+int Correto(int *, int, int, int, int);
 
 
 int main(int argc, char *argv[]) {
@@ -48,23 +48,23 @@ int main(int argc, char *argv[]) {
 
     // dump tabuleiro inicial
     DumpTabul(tabulIn, tam, 1, tam, "Inicial", tamLocal, linha, myId, numProc);
+    for (i=0; i<2*(tam-3); i++) {
+      UmaVida(tabulIn, tabulOut, tam, tamLocal, linha, myId, numProc);
+      sprintf(msg,"Iter %3.3d\0", 2*i+1);
+      DumpTabul(tabulIn, tam, 1, tam, msg, tamLocal, linha, myId, numProc);
+
+      UmaVida(tabulOut, tabulIn, tam, tamLocal, linha, myId, numProc);
+      sprintf(msg,"Iter %3.3d\0", 2*i+2);
+      DumpTabul(tabulIn, tam, 1, tam, msg, tamLocal, linha, myId, numProc);
+     }
+
     Correto(tabulIn, tam, myId, tamLocal, linha);
 
    free(tabulIn);
    free(tabulOut);
   }
 
-  // for (i=0; i<2*(tam-3); i++) {
-
-    // UmaVida(tabulIn, tabulOut, tam);
-    // sprintf(msg,"Iter %3.3d\0", 2*i+1);
-    // DumpTabul(tabulOut, tam, 1, tam, msg);
-
-    // UmaVida(tabulOut, tabulIn, tam);
-    // sprintf(msg,"Iter %3.3d\0", 2*i+2);
-    // DumpTabul(tabulIn, tam, 1, tam, msg);
-  // }
-
+ 
   // if (Correto(tabulIn, tam)) 
     // printf("**RESULTADO CORRETO**\n");
   // else
